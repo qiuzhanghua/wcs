d5e32b32c005c82e9f0cf8320292d5f5d9fefef6
qiuzhanghua <qiuzhanghua@icloud.com>
2023-05-11 16:27:17 +0800

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Web Components with Svelte
+
+## Installation
+```bash
+pnpm install
+ncu -n
+pnpm install
+pnpm build
+```
+
+
+## Running the app
+```bash
+open index.html
+```
---
+++ src/Component.svelte
@@ -0,0 +1,23 @@
+<svelte:options tag="info-card" />
+<div id="app" bind:this={self}>
+	<h1 class="name">{name}</h1>
+	<button on:click={sayMyName}>呼唤我的名字</button>
+</div>
+
+<script lang="ts">
+	export let name
+	export let self
+	function sayMyName() {
+		alert(name)
+		// 呼唤结束，你还可以自定义事件，冒泡向上层传递信息
+		self.dispatchEvent(new CustomEvent('done', {
+			detail: 'anything'
+		}))
+	}
+</script>
+
+<style>
+	#app .name {
+		color: red;
+	}
+</style>
