b5a121ff676882fd7039ba5367827c89592ed81d
qiuzhanghua <qiuzhanghua@icloud.com>
2023-05-12 14:24:51 +0800

    fix some lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import pkg  from './package.json' assert { type: 'json' };
 import typescript from '@rollup/plugin-typescript';
-
-const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
 	input: 'src/index.ts',
